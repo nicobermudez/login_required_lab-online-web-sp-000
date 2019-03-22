325a44304f899247d8f2f1404257ae68afe8d937
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-22 16:01:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def current_user
     session[:name]
   end
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
 end
