5887d0b78c31948a76232918c35e024db8bb5d9d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-22 16:02:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
-  def new 
+  
+  def new
   end
+
   def create
     return redirect_to(controller: 'sessions',
                        action: 'new') if !params[:name] || params[:name].empty?
